5a76330136ccffa218c1b622f7c5118e036cca92
stdviv-m <vivmukhongo@gmail.com>
2024-02-17 00:49:09 +0300

    changed the z index

--- bulb/main.js
+++ bulb/main.js
@@ -1,12 +1,12 @@
-
-
-
+const $shadow = document.querySelector('shadow');
+const $light = document.querySelector('bulb');
+const $filament = document.querySelector('filament')
 
 $light.addEventlistener{
     'click',
     () =>{
         $light.classlist.toggle('off');
         $shadow.classlist.toggle('off');
-        $filaments.classlist.list.toggle('off');
+        $filament.classlist.list.toggle('off');
     };
 };
